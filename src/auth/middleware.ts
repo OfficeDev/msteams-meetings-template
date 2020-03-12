8d935152d5fa66c75363c417abe25b96fb1650de
Adrian Solis <aosolis@microsoft.com>
2020-03-12 12:26:12 -0700

    Handle errors from loginPopup: - Log the error and remain in the same state so the user can try to sign in again.
    Tested with:
    - Canceled signin
    - Click "Sign in" multiple times in quick succession

--- src/auth/middleware.ts
+++ src/auth/middleware.ts
@@ -1,13 +1,12 @@
-import { Middleware } from "redux";
+import { Middleware } from 'redux';
 import { msalApp } from './msalApp'
 import { OPEN_SIGNIN_DIALOG_COMMAND, SIGNIN_COMPLETE_EVENT, SigninCompleteEvent } from './actions'
-import { push } from "connected-react-router";
+import { push } from 'connected-react-router';
 
 export function createAuthMiddleware() : Middleware
 {
     return store => next => action => {
-        if (action.type == OPEN_SIGNIN_DIALOG_COMMAND) {
-            // TODO: handle multiple logins
+        if (action.type === OPEN_SIGNIN_DIALOG_COMMAND) {
             msalApp
                 .loginPopup({
                     scopes: [
@@ -15,17 +14,19 @@ export function createAuthMiddleware() : Middleware
                     ]
                 })
                 .then((response) => {
-                    console.log('AUTH RESPONSE', response);
+                    console.log('Login succeeded:');
                     store.dispatch({
                         type: SIGNIN_COMPLETE_EVENT,
                         idToken: response.idToken
-                    } as SigninCompleteEvent)
+                    } as SigninCompleteEvent);
                 })
-                // TODO: handle auth failures
+                .catch((error) => {
+                    console.log('Login failed:', error);
+                });
         }
 
-        if (action.type == SIGNIN_COMPLETE_EVENT) {
-            store.dispatch(push("/createLanding"))
+        if (action.type === SIGNIN_COMPLETE_EVENT) {
+            store.dispatch(push('/createLanding'));
         }
 
         next(action);
