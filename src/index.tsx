4cd63dd6544fc0ce5367130aacd032cf17354628
Nick Beaulieu <b41ulu@gmail.com>
2020-03-16 08:38:55 -0700

    Updated MSAL detection logic

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+function isMsalRedirect()
+{
+    const msalParameters = ["access_token", "id_token", "code", "error", "error_description"];
+    const hash = window.location.hash;
+    return msalParameters.some(param => hash.indexOf(param) >= 0);
+}
+
 // hack to fix MSAL fighting with hash routing
-if (window.location.hash.indexOf("token") < 0 || window.location.hash.indexOf("error_message") < 0) {
+if (!isMsalRedirect()) {
     ReactDOM.render(<App />, document.getElementById('root'));
 }
