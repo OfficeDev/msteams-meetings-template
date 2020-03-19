10e7f146cd11180baab880d575b0d419c8cbcd34
Adrian Solis <aosolis@microsoft.com>
2020-03-18 23:41:32 -0700

    Update graphic on the error page

--- src/ErrorPage.tsx
+++ src/ErrorPage.tsx
@@ -6,6 +6,8 @@ import { connect } from 'react-redux';
 import { goBack } from 'connected-react-router';
 import { OnlineMeetingInput } from './meeting-creator/models';
 import { CREATE_MEETING_COMMAND, CreateMeetingCommand } from './meeting-creator/actions';
+import { Header } from './components/header';
+import errorImage from './images/genericError.svg';
 
 const semiboldStyle = { root: { fontWeight: FontWeights.semibold } };
 
@@ -26,46 +28,49 @@ const mapDispatchToProps = (dispatch: Dispatch) => ({
       type: CREATE_MEETING_COMMAND,
       fromPage: "error",
       meeting,
-    } as CreateMeetingCommand)
+    } as CreateMeetingCommand);
   }
 });
 
 function ErrorPageComponent(props: ErrorPageProps) {
   return (
-    <Stack
-      className="container"
-      horizontalAlign="center"
-      verticalAlign="center"
-      verticalFill
-      tokens={{
-        childrenGap: 35
-      }}>
-      <img
-        className="splashImage"
-        src="https://statics.teams.microsoft.com/hashedassets-launcher/launcher_meetings_new.b2c45282207c2dff1f96b89f128a7e31.svg"
-        alt="logo"
-      />
-      <Text variant="xxLarge" styles={semiboldStyle}>
-        Oops! Your meeting wasn't created successfully.
-      </Text>
-      <Text variant="large" className="uTextCenter">
-        Please try again. If the problem persists, check with your IT administrator to ensure you have the proper permissions.
-      </Text>
-      <Stack horizontal tokens={{childrenGap: 10}}>
-        <DefaultButton
-          className="teamsButtonInverted"
-          text="Back"
-          onClick={(event) => props.goBack()} 
-          ariaLabel="Back to last screen"
+    <>
+      <Header />
+      <Stack
+        className="container"
+        horizontalAlign="center"
+        verticalAlign="center"
+        verticalFill
+        tokens={{
+          childrenGap: 35
+        }}>
+        <img
+          className="splashImage"
+          src={errorImage}
+          alt="Error graphic"
         />
-        <PrimaryButton
-          className="teamsButton"
-          primary text="Try again" 
-          onClick={(event) => props.createMeeting(props.meeting)} 
-          ariaLabel="Try again"
-        />
+        <Text variant="xxLarge" styles={semiboldStyle}>
+          Oops! Your meeting wasn't created successfully.
+        </Text>
+        <Text variant="large" className="uTextCenter">
+          Please try again. If the problem persists, check with your IT administrator to ensure you have the proper permissions.
+        </Text>
+        <Stack horizontal tokens={{childrenGap: 10}}>
+          <DefaultButton
+            className="teamsButtonInverted"
+            text="Back"
+            onClick={() => props.goBack()} 
+            ariaLabel="Back to last screen"
+          />
+          <PrimaryButton
+            className="teamsButton"
+            primary text="Try again" 
+            onClick={() => props.createMeeting(props.meeting)} 
+            ariaLabel="Try again"
+          />
+        </Stack>
       </Stack>
-    </Stack>
+    </>
   );
 }
 
