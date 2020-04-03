7ace3a3d1c6ba9d5c8398016a7f74662ccfce665
Adrian Solis <Adrian.Solis@microsoft.com>
2020-04-03 22:21:44 +0000

    Merged PR 534: Fix loading of strings
    - Remove case conflict in checked in translation files
    - Handle neutral locales, and fallback to neutral locales
    - Remove caching of translations in sessionStorage

--- src/localization/translate.ts
+++ src/localization/translate.ts
@@ -1,121 +1,136 @@
 import { createIntl, createIntlCache } from 'react-intl';
-import _ from 'lodash';
 
-const supportedLocales = [
-  'ar',
-  'bg',
-  'cs',
-  'cy',
-  'da',
-  'de',
-  'en-us',
-  'en-gb',
-  'es',
-  'es-mx',
-  'fi',
-  'fr',
-  'fr-ca',
-  'he',
-  'is',
-  'it',
-  'ja',
-  'ko',
-  'nb',
-  'nl',
-  'no',
-  'nn-no',
-  'pl',
-  'pt-br',
-  'pt-pt',
-  'ru',
-  'sv',
-  'th',
-  'tr',
-  'zh-cn',
-  'zh-tw'
-];
+const defaultLocale = 'en-US';
+const supportedLocalesAndFallbackMap: Record<string, string> = {
+  'ar': 'ar-SA',
+  'ar-sa': 'ar-SA',
+  'bg': 'bg-BG',
+  'bg-bg': 'bg-BG',
+  'cs': 'cs-CZ',
+  'cs-cz': 'cs-CZ',
+  'cy': 'cy-GB',
+  'cy-gb': 'cy-GB',
+  'da': 'da-DK',
+  'da-dk': 'da-DK',
+  'de': 'de-DE',
+  'de-de': 'de-DE',
+  'en': 'en-US',
+  'en-gb': 'en-GB',
+  'en-us': 'en-US',
+  'es': 'es-ES',
+  'es-es': 'es-ES',
+  'es-mx': 'es-MX',
+  'fi': 'fi-FI',
+  'fi-fi': 'fi-FI',
+  'fr': 'fr-FR',
+  'fr-fr': 'fr-FR',
+  'fr-ca': 'fr-CA',
+  'he': 'he-IL',
+  'he-il': 'he-IL',
+  'is': 'is-IS',
+  'is-is': 'is-IS',
+  'it': 'it-IT',
+  'it-it': 'it-IT',
+  'ja': 'ja-JP',
+  'ja-jp': 'ja-JP',
+  'ko': 'ko-KR',
+  'ko-kr': 'ko-KR',
+  'nb': 'nb-NO',
+  'nb-no': 'nb-NO',
+  'nl': 'nl-NL',
+  'nl-nl': 'nl-NL',
+  'nn': 'nn-NO',
+  'nn-no': 'nn-NO',
+  'no': 'nb-NO',
+  'pl': 'pl-PL',
+  'pl-pl': 'pl-PL',
+  'pt': 'pt-BR',
+  'pt-br': 'pt-BR',
+  'pt-pt': 'pt-PT',
+  'ru': 'ru-RU',
+  'ru-ru': 'ru-RU',
+  'sk': 'sk-SK',
+  'sk-sk': 'sk-SK',
+  'sr': 'sr-Latn-RS',
+  'sr-latn-rs': 'sr-Latn-RS',
+  'sv': 'sv-SE',
+  'sv-se': 'sv-SE',
+  'th': 'th-TH',
+  'th-th': 'th-TH',
+  'tr': 'tr-TR',
+  'tr-tr': 'tr-TR',
+  'zh': 'en-US',
+  'zh-cn': 'zh-CN',
+  'zh-tw': 'zh-TW',
+};
 
 export function getUserLocale() {
-  let locale = [];
-
   if (navigator.languages) {
-    locale.push(navigator.languages[0].toLocaleLowerCase());
+    return navigator.languages[0].toLowerCase();
   } else if (navigator.language) {
-    locale.push(navigator.language.toLocaleLowerCase());
+    return navigator.language.toLowerCase();
   } else {
-    locale.push('en-us');
+    return defaultLocale;
   }
-
-  return locale[0];
 }
 
-const userLocale = getUserLocale();
-const sku = process?.env?.REACT_APP_SKU ?? 'edu';
-
-async function loadTranslations() {
-  let translations;
-
-  if (_.includes(supportedLocales, userLocale)) {
-    translations = await import(`../translations/${sku}/${userLocale}/strings.json`);
+function getTranslationLocale(locale: string) {
+  if (supportedLocalesAndFallbackMap.hasOwnProperty(locale)) {
+    return supportedLocalesAndFallbackMap[locale];
+  } else if (locale.indexOf('-') > -1) {
+    const index = locale.indexOf('-');
+    const languageCode = locale.substring(0, index);
+    return supportedLocalesAndFallbackMap.hasOwnProperty(languageCode) ? supportedLocalesAndFallbackMap[languageCode] : defaultLocale;
   } else {
-    translations = await import(`../translations/${sku}/en-us/strings.json`);
+    return defaultLocale;
   }
-
-  return translations;
 }
 
-loadTranslations().then((translatedStrings: any) => {
-  sessionStorage.setItem(
-    'translatedStrings',
-    JSON.stringify(translatedStrings.default)
-  );
-});
+async function loadTranslations(sku: string, userLocale: string) {
+  const translationLocale = getTranslationLocale(userLocale);
+  console.log(`Using ${translationLocale} strings for ${userLocale} in ${sku}`);
 
-let storedTranslations = sessionStorage.getItem('translatedStrings');
+  let stringsModule: any;
+  try {
+    stringsModule = await import(`../translations/${sku}/${translationLocale}/strings.json`);
+  } catch (e) {
+    console.error(`Failed to load ${translationLocale} in ${sku}, falling back to en-US`);
+    stringsModule = await import(`../translations/${sku}/en-us/strings.json`);
+  }
 
-export let messages: any = storedTranslations
-  ? JSON.parse(storedTranslations)
-  : {};
+  return stringsModule.default;
+}
 
 const cache = createIntlCache();
-let int = createIntl(
-  {
-    locale: userLocale,
-    messages: messages
-  },
-  cache
-);
+let intl: any;
 
 export async function getMessages(): Promise<any>
 {
-  if (!!storedTranslations)
-  {
-    return JSON.parse(storedTranslations);
+  const sku = process?.env?.REACT_APP_SKU ?? 'edu';
+  const userLocale = getUserLocale();
+
+  if (intl && (intl.locale === getUserLocale())) {
+    return intl.messages;
   }
-  console.log(`Translation not cached, loading ${userLocale} for ${sku}`);
-  await loadTranslations();
 
-  // Load the translations into the storedTranslations variable again.
-  storedTranslations = sessionStorage.getItem('translatedStrings');
-
-  if (!storedTranslations)
+  console.log(`Loading ${userLocale} strings in ${sku}`);
+  const messages = await loadTranslations(sku, userLocale);
+  if (!messages)
   {
-    console.error("Unable to load translations");
+    console.error(`Unable to load ${userLocale} strings in ${sku}`);
     return {};
   }
 
-  messages = JSON.parse(storedTranslations);
-  int = createIntl(
+  intl = createIntl(
     {
       locale: userLocale,
       messages: messages
     },
     cache
-  );
-  
+  );  
   return messages;
 }
 
 export const translate = (id: string, values?: {}) => {
-  return int.formatMessage({ id }, values);
+  return intl.formatMessage({ id }, values);
 };
